1944bc79cbb6a68eeb6d53f4c2721229fecc573e
Seung Chi <seungyoonchi@gmail.com>
2017-10-13 12:10:00 -0700

    Add comments to scrape ctrl js

--- Web/Scripts/ScrapeCtrl.js
+++ Web/Scripts/ScrapeCtrl.js
@@ -12,23 +12,21 @@
         var vm = this;
         vm.$scope = $scope;
         vm.scrapeService = scrapeService;
-        vm.$onInit = _init;
         vm.keyword = "";
         vm.items = [];
         vm.$window = $window;
         vm.search = _search;
-        vm.reset = false;
-        vm.load = false;
+        vm.reset = false; //Reset page for new entry
+        vm.load = false; //Loading shows or not
 
-        function _init() {
-        }
-
+        //Search button function
         function _search() {
             vm.load = true;
             if (vm.reset == true) vm.items = [];
             return vm.scrapeService.get(vm.keyword)
                 .then(_success, _error);
         }
+        //On success, images set to items to show on page
         function _success(data) {
             console.log(data.data);
             vm.items = data.data;
@@ -37,6 +35,7 @@
             vm.load = false;
             return 
         }
+        //Error logs message in console
         function _error(err) {
             return console.log(err)
         }
